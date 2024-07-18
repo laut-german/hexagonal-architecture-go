98b3a789af34e8dd7bb0dc5a0c97608953755153
lautaro-zuniga <laut.german@gmail.com>
2024-07-18 18:24:33 +0200

    update migrate up schema

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -1,17 +1,19 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE users (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE carts (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE cart_items (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     cart_id UUID REFERENCES carts(id) ON DELETE CASCADE,
     product_id VARCHAR(255) NOT NULL,
     quantity INT NOT NULL
